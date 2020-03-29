3e013f82cf9e08e3c9656401d355fcd98c0fac05
BYY-Grend <2735777529@qq.com>
2020-03-29 17:33:38 +0800

    登录

---
+++ ihome_4.0/ihome/constants.py
@@ -0,0 +1,18 @@
+# 图片验证码的Redis有效期
+IMAGE_CODE_REDIS_EXPIRES = 180
+
+# 短信验证码的Redis有效期
+SMS_CODE_REDIS_EXPIRES = 300
+
+# 发送短信验证码的间隔
+SEND_SMS_CODE_INTERVAL = 60
+
+# 登录错误尝试次数
+LOGIN_ERROR_MAX_TIMES = 5
+
+# 登录错误限制的时间
+LOGIN_ERROR_FORBIO_TIME = 600
+
+
+
+
